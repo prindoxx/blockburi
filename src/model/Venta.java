07983d98cda09bd30fcaebb9b935abc1a503e1bb
prindoxx <pablortiz132@gmail.com>
2022-05-21 17:03:39 -0400

    Se pueden comprar peliculas y las muestra el reporte txt
    La venta de peliculas se guardará en un mapa mientras el programa s esté ejecutando, despues se descartará

--- src/model/Venta.java
+++ src/model/Venta.java
@@ -4,8 +4,10 @@
  */
 package model;
 
+import app.VentaPelicula;
 import java.sql.SQLException;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -31,12 +33,57 @@ public class Venta extends Transaccion{
         this.fechaVenta = fechaVenta;
     }
     
-
-
-    public Transaccion arrendarPelicula() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+    public Venta comprarPelicula(HashMap<Integer, Prestamo> mapaPrestamos, HashMap<String, Trabajador> mapaTrabajadores, HashMap<Integer, Pelicula> mapaComprarPelis, HashMap<Integer, Venta> mapaVentas, Pelicula oPeliculaArrendar, String rutTrabajador, String rutCliente, String fechaVenta ) {
     
+        Trabajador oTrabajador = new Trabajador();
+        oTrabajador.setRut(rutTrabajador);
+        if ( oTrabajador.trabajadorExiste(mapaTrabajadores, rutTrabajador) ){
+            //existe el trabajador en el mapa
+            
+            //int numeroRandom = (int)(Math.random()*1000000000+1);//numero random creado para usar de ID
+            int total=0;
+            Venta oVenta = new Venta();
+            HashMap<Integer, Pelicula> mapaPeliculasArrendar = mapaComprarPelis;
+            
+            mapaPeliculasArrendar.put(oPeliculaArrendar.getId(), oPeliculaArrendar);
+            
+            for (Map.Entry<Integer, Pelicula> entry : mapaPeliculasArrendar.entrySet()) {
+                Pelicula oPelicula1 = entry.getValue();
+                
+                System.out.println("ID PELICULA MAPA: "+oPelicula1.getId());
+                System.out.println("NOMBRE PELICULA MAPA: "+oPelicula1.getNombre());
+                
+            }
+            
+            //oPrestamo.setIdPrestamo(numeroRandom);
+            oVenta.setRutCliente(rutCliente);
+            oVenta.setRutTrabajador(rutTrabajador);
+            oVenta.mapaPeliculas = mapaPeliculasArrendar;
+            oVenta.setFechaVenta(fechaVenta);
+            
+            for (Map.Entry<Integer, Pelicula> entry : mapaPeliculasArrendar.entrySet()) {
+                
+                Pelicula oPeliculaM = entry.getValue();
+                
+                total += oPeliculaM.getPrecioArriendo();
+                
+            }
+            
+            oVenta.setMontoAPagar(total);
+            
+            //System.out.println("idPrestamo: "+oPrestamo.getIdPrestamo());
+            
+            return oVenta;
+            
+        } else {
+            //el trabajador no existe en el mapa
+            VentaPelicula oVentaPelicula = new VentaPelicula(mapaPeliculas, mapaPrestamos, mapaTrabajadores,mapaVentas);
+            JOptionPane.showMessageDialog(oVentaPelicula, "Ingrese un RUT de Trabajador valido");
+            return null;
+        }
+        
+        
+    }
     @Override
     public void infoVenta() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
